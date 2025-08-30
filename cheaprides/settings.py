f071f972e0f4970cd4ba5887560dbf707e94a6f0
Prince Owusu <powusu050@gmail.com>
2025-08-30 18:56:17 +0000

    new updates

--- cheaprides/settings.py
+++ cheaprides/settings.py
@@ -117,9 +117,9 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 USE_TZ = True
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATIC_URL = '/static/'
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 # MEDIA_URL = '/media/'
